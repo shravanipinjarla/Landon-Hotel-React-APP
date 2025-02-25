8ab53587dd4957bca319e6edbb8e130ebab71bc1
Shravani Pinjarla <shravanipinjarla@Shravanis-MacBook-Air.local>
2025-02-25 18:40:05 -0500

    Added json to represent data and made components dynamic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+import Footer from './components/Footer.js'
+import Header from './components/Header.js'
+import Main from './components/Main.js'
+
+class App extends React.Component {
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React in a day!
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
+  }
 }
 
 export default App;
